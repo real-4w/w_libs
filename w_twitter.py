37eefbc67157b8e5810ad291e9f01f32fa477b8a
real-4w <real_4w@protonmail.com>
2021-11-14 15:14:11 +1300

    New method for TwitterNew Class

--- w_twitter.py
+++ w_twitter.py
@@ -25,7 +25,7 @@ class TwitterSource(Source):
         pass
 
 class TwitterNew(TwitterSource):
-    """Create a class for connectin to Twitter's API.
+    """Create a class for working with Twitter's API.
 
     Args:
         w_api_key (str): Twitter API Key
@@ -46,16 +46,20 @@ class TwitterNew(TwitterSource):
                
     def fetch(self):
         pass
-
-    def w_tweet(self, w_tweet_txt: str):
-        try:
-            self.twitter_api.PostUpdate(f"{w_tweet_txt}")
-        except UnicodeDecodeError:
-            print("Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
-            print("Try explicitly specifying the encoding with the --encoding flag")
-            sys.exit(2)
-        #print("{0} just posted: {1}".format(status.user.name, status.text))  
     
+    def w_get_user_info (self, w_user: str) -> twitter.models.User:
+        """Function to return Twitter user detais.
+
+        Args:
+            w_user (str): Twitter user name to be returned
+        Returns:
+            twitter.models.User   
+        """
+        w_user_api = self.twitter_api.GetUser(screen_name=w_user)
+        print(f"Twitter handle: {w_user_api.screen_name} has tweeted {w_user_api.statuses_count} tweets. {w_user_api.screen_name} is following {w_user_api.friends_count} users and is followed by {w_user_api.followers_count} users.")
+        print(type(w_user_api))
+        return(w_user_api)
+
     def w_send_random_tweet(self, w_tweet_text: str): 
         """Function to create a random tweet, followed by the provided text.
         Will build a random sentence: Names + extras + verbs + nounds.
@@ -66,3 +70,17 @@ class TwitterNew(TwitterSource):
         self.random_tweet = f"{secrets.choice(self.names)} {secrets.choice(self.extras)} {secrets.choice(self.verbs)} {secrets.choice(self.nouns)}{secrets.choice(self.infinitives)}: {w_tweet_text}"
         self.w_tweet(self.random_tweet)
         print(self.random_tweet)
+    
+    def w_tweet(self, w_tweet_txt: str):
+        """Function to send a tweet with the provided text.
+        
+        Args:
+            w_tweet_text (str): This will be tweeted.
+        """
+        try:
+            self.twitter_api.PostUpdate(f"{w_tweet_txt}")
+        except UnicodeDecodeError:
+            print("Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
+            print("Try explicitly specifying the encoding with the --encoding flag")
+            sys.exit(2)
+        #print("{0} just posted: {1}".format(status.user.name, status.text))  
