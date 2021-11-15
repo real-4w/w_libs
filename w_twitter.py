5e667204423bbc14f1ca88fd911f300fd7fecb32
real-4w <real_4w@protonmail.com>
2021-11-16 08:58:07 +1300

    Updates to Twitter classes/file.

--- w_twitter.py
+++ w_twitter.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-import twitter, sys, secrets
+import twitter, sys, secrets, re, webbrowser
 
 class Source(ABC):
   
@@ -41,13 +41,31 @@ class TwitterNew(TwitterSource):
         self.extras=["surely", "probably", "likely","definately", "absolutely", ""]
         self.verbs=["was","is","looks","seems", "could be", "might be", "should be", "looks like it will be"]
         self.nouns=["cool","exciting","pretty cool","awesome","pretty exicting", "funky", "usefull", "mighty good"]
-        self.infinitives = ["", ", for no apparent reason", ", because I say so", ", just because", "", "since it is one of thoase days"]
+        self.infinitives = ["", ", for no apparent reason", ", because I say so", ", just because", "", " since it is one of thoase days"]
         self.random_tweet = "This will overwritten by the methods."
                
     def fetch(self):
         pass
     
-    def w_get_user_info (self, w_user: str) -> twitter.models.User:
+    def w_get_tweets(self, w_user: str, w_count: int, w_open: bool):
+        """Gets the last <w_count> tweets from <w_user} and can open any links in there.
+
+        Args:
+            w_user (str): Twitter handle as a string
+            w_count (int): Number of tweets to get
+            w_open (bool): Do we open the links in the tweets.
+        """
+        w_tweets = self.twitter_api.GetUserTimeline(screen_name=w_user, count=w_count)
+        w_p_url = re.compile(r'http\S+')
+        for w_tweet in w_tweets:
+            print(f"Tweet: {w_tweet.text}")
+            if w_open == True:
+                w_url = w_p_url.findall(w_tweet.text)
+                if len(w_url) > 0: 
+                    for w_tab in w_url : 
+                        webbrowser.open_new(w_tab)
+    
+    def w_get_user_info (self, w_user: str): #-> twitter.models.User:
         """Function to return Twitter user detais.
 
         Args:
@@ -56,9 +74,9 @@ class TwitterNew(TwitterSource):
             twitter.models.User   
         """
         w_user_api = self.twitter_api.GetUser(screen_name=w_user)
-        print(f"Twitter handle: {w_user_api.screen_name} has tweeted {w_user_api.statuses_count} tweets. {w_user_api.screen_name} is following {w_user_api.friends_count} users and is followed by {w_user_api.followers_count} users.")
+        print(f"Twitter handle: {w_user_api.screen_name} has been tweeting since {w_user_api.created_at} and tweeted {w_user_api.statuses_count} tweets.\n{w_user_api.screen_name} is following {w_user_api.friends_count} users and is followed by {w_user_api.followers_count} users.")
         #print(type(w_user_api))
-        return(w_user_api)
+        #return(w_user_api)
 
     def w_send_random_tweet(self, w_tweet_text: str): 
         """Function to create a random tweet, followed by the provided text.
