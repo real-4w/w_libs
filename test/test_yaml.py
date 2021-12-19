9376da4fad8a34ca5bfaa7b22984f0d8e80a96ab
Real-4W <real_4w@protonmail.com>
2021-12-19 14:51:46 +1300

    unit testing introduced

--- test/test_yaml.py
+++ test/test_yaml.py
@@ -3,12 +3,24 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardi
 import w_yaml as w_y
 import unittest
 
-class Test_W_Yaml(unittest.TestCase):
-    def test_ProcessYAML(self):
-        debug, yaml_data = w_y.ProcessYAML('portfolio.yaml') 
-        assert(debug, False)
+class test_w_yaml(unittest.TestCase):
+    debug, yaml_data = w_y.ProcessYAML('portfolio.yaml') 
+    def test_processyaml(self):
+        self.assertEqual(self.debug, False , "Debug needs to be True or False")
+        
+    def test_keys(self):
+        api_key = self.yaml_data['api_key']
+        api_key_dev = self.yaml_data['api_key_dev']
+        self.assertIn(api_key, "8C7LNHUO0MZEPUTC")
+        self.assertIn(api_key_dev, "4OYY6AT2N9B0WB5X")
+        
+    def test_crypto(self):
+        pass
+
+    def test_property(self):
+        pass    
+
 
 if __name__ == "__main__":
     unittest.main()
-    #debug, yaml_data = w_y.ProcessYAML('portfolio.yaml') 
-    #print(f"Debug: {debug}") 
+    
