b4b002784fafa09bc3bd4831ad36d451d0f1c466
Real-4W <real_4w@protonmail.com>
2021-12-19 14:22:34 +1300

    WIP

--- test/test_yaml.py
+++ test/test_yaml.py
@@ -1,5 +1,14 @@
 import os, sys, pathlib
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))  #needed to import libs from project directory
 import w_yaml as w_y
+import unittest
+
+class Test_W_Yaml(unittest.TestCase):
+    def test_ProcessYAML(self):
+        debug, yaml_data = w_y.ProcessYAML('portfolio.yaml') 
+        assert(debug, False)
+
 if __name__ == "__main__":
-    debug, yaml_data = w_y.ProcessYAML('portfolio.yaml')  
+    unittest.main()
+    #debug, yaml_data = w_y.ProcessYAML('portfolio.yaml') 
+    #print(f"Debug: {debug}") 
